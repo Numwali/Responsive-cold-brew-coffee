e394745aa52eaa91b6b5fb0bf8c75b22a9488cb7
Numwali <n.umwali@alustudent.com>
2024-12-03 12:51:34 +0200

    updated version of javascript

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let menu = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.navlist');
+let menu=document.querySelector('#menu-icon');
+let navlist=document.querySelector('.navlist');
 
-menu.onclick = () => {
-  menu.classList.toggle('bx-x');
-  navlist.classList.toggle('open');
+menu.onclick=()=>{
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open')
 }
